7701b0a862dcf9cda2035de1e064c29b60962cc8
Nicholas <nicholasjoven@gmail.com>
2022-07-18 12:50:45 -0400

    finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  /* write an <About> component here */
+  return (
+    <div id='about'>
+      <h1>About</h1>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      { NavBar() }
+      { Home() }
+      { About() }
     </div>
   );
 }
